fdfa335e98042190097f423656312c67df9213a2
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2022-09-07 22:08:55 +0100

    Added ThemeProvider, GlobalStyle and styled Root.js

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import UsersList from 'components/UsersList/UsersList'
+import UsersList from 'components/organisms/UsersList/UsersList'
+import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({theme}) => theme.colors.lightgrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 
 const Root = () => (
-  <>
-    <UsersList />
-  </>
+
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Wrapper>
+      <UsersList />
+    </Wrapper>
+  </ThemeProvider>
+
+
 );
 
 export default Root;
