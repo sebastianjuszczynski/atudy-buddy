72f81b6bc061178fadbc4979b671abef5c520247
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2022-11-12 18:39:11 +0000

    value changed inConsent

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -41,7 +41,7 @@ const AddUser = () => {
             <FormField label='Name' id='name' name='name' value={formValues.name} onChange={handleInputChange}></FormField>
             <FormField label='Attendance' id='attendance' name='attendance' value={formValues.attendance} onChange={handleInputChange}></FormField>
             <FormField label='Average' id='average' name='average' value={formValues.average} onChange={handleInputChange}></FormField>
-            <FormField label='Consent' id='consent' name='consent' type="checkbox" value={formValues.average} onChange={handleToggleConsent}></FormField>
+            <FormField label='Consent' id='consent' name='consent' type="checkbox" value={formValues.consent} onChange={handleToggleConsent}></FormField>
             <Button type='submit'>Add</Button>
             {formValues.error ? <p>{formValues.error}</p> : null}
         </ViewWrapper>
