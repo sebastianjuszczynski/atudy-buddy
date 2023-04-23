a70e9e300741946dcdbe4a2d4ec8af6e8db99064
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2023-04-23 14:45:14 +0100

    fixed the issues with AddUser tests

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import AddUser from './AddUser';
 import Dashboard from './Dashboard';
+import '@testing-library/jest-dom/extend-expect';
 import { screen, fireEvent } from '@testing-library/react';
 import { renderWithProviders } from 'helpers/renderWithProviders'
 
 describe('Form Field', () => {
-    it('Render the component', () => {
+    it('Adds new user to the list', () => {
+        renderWithProviders(
+            <>
+                <AddUser />
+                <Dashboard />
+            </>
+        );
+        fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Sebus' } });
+        fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+        fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
+        fireEvent.click(screen.getByTestId('Consent'));
+        fireEvent.click(screen.getByText('Add'));
+        screen.getByText('Sebus');
+    });
+
+    it('Prevents adding new user if the consent is not checked', () => {
         renderWithProviders(
             <>
                 <AddUser />
@@ -16,6 +32,7 @@ describe('Form Field', () => {
         fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
         fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
         fireEvent.click(screen.getByText('Add'));
-        screen.getByText('Sebus');
-      });
+        const newUser = screen.queryByText('Sebus');
+        expect(newUser).not.toBeInTheDocument();
     });
+});
