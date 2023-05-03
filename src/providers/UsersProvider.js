ed532ea5e9b450f5200553257a7d691b3407ae8d
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2023-05-03 19:24:34 +0100

    SearchBar tests added

--- src/providers/UsersProvider.js
+++ src/providers/UsersProvider.js
@@ -1,43 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 export const UsersContext = React.createContext({
-    users: [],
-    handleAddUser: () => { },
-    deleteUser: () => { },
+  users: [],
+  handleAddUser: () => {},
+  deleteUser: () => {},
 });
 
-const UsersProviders = ({ children }) => {
-    const [users, setUsers] = useState([]);
+const UsersProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        axios.get('/students')
-        .then(({data}) => setUsers(data.students))
-        .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    axios
+      .get('/students')
+      .then(({ data }) => setUsers(data.students))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const deleteUser = (name) => {
-        const filteredUsers = users.filter((user) => user.name !== name);
-        setUsers(filteredUsers);
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const handleAddUser = (values) => {
+    const newUser = {
+      name: values.name,
+      attendance: values.attendance,
+      average: values.average,
     };
-
-    const handleAddUser = (Values) => {
-        const newUser = {
-            name: Values.name,
-            attendance: Values.attendance,
-            average: Values.average,
-        };
-        setUsers([newUser, ...users]);
-    };
-    return (
-        <UsersContext.Provider value={{
-            users,
-            handleAddUser,
-            deleteUser,
-        }}>
-            {children}
-        </UsersContext.Provider>
-    )
+    setUsers([newUser, ...users]);
+  };
+  return (
+    <UsersContext.Provider
+      value={{
+        users,
+        handleAddUser,
+        deleteUser,
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  );
 };
-export default UsersProviders;
+
+export default UsersProvider;
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import StudentsList from 'components/organisms/StudentsList/StudentsList';
@@ -7,29 +7,37 @@ import { GroupWrapper, TitleWrapper, Wrapper } from 'views/Dashboard.styles';
 import { Title } from 'components/atoms/Title/Title';
 
 const Dashboard = () => {
-    const { groups } = useStudents();
-    const { id } = useParams();
+  const [groups, setGroups] = useState([]);
+  const { getGroups } = useStudents();
+  const { id } = useParams();
 
-    if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0]}`} />;
+  useEffect(() => {
+    (async () => {
+      const groups = await getGroups();
+      setGroups(groups);
+    })();
+  }, [getGroups]);
+
+  if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0]}`} />;
 
 
-    return (
-        <Wrapper>
-        <TitleWrapper>
-          <Title as="h2">Group {id}</Title>
-          <nav>
-            {groups.map((group) => (
-              <Link key={group} to={`/group/${group}`}>
-                {group}{' '}
-              </Link>
-            ))}
-          </nav>
-        </TitleWrapper>
-        <GroupWrapper>
-          <StudentsList />
-        </GroupWrapper>
-      </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        <Title as="h2">Group {id}</Title>
+        <nav>
+          {groups.map((group) => (
+            <Link key={group} to={`/group/${group}`}>
+              {group}{' '}
+            </Link>
+          ))}
+        </nav>
+      </TitleWrapper>
+      <GroupWrapper>
+        <StudentsList />
+      </GroupWrapper>
+    </Wrapper>
+  );
 };
 
 export default Dashboard;
