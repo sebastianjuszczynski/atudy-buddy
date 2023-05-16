4dc08d5004dd595c208ced7dbc2638cfcab8aece
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2023-05-16 13:16:42 +0100

    Created AppProviders.js and implement in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import Root from 'views/Root';
 import 'assets/styles/fonts.css';
 import { worker } from 'mocks/browsers';
+import AppProviders from 'providers/AppProviders';
 
 worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Root />
+    <AppProviders>
+      <Root />
+    </AppProviders>
   </React.StrictMode>
 );
